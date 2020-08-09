607768109b87718896bf8b4dc7ace9136ca6c063
thexiang <xxiang78@gmail.com>
2020-08-09 13:25:12 -0400

    init

---
+++ jenkins+gitlab+synk/start.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt  install docker.io
+
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
+
+sudo sysctl -w vm.max_map_count=262144
+
+sudo docker-compose up -d
